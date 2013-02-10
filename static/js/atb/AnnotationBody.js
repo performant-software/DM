33a88c234dd5b58836e11d6e0aec64b979322235
Tim Andres <tandres@drew.edu>
2013-02-10 16:36:24 -0500

    Removed workspace and accounts apps' static files from base static folder

--- static/js/atb/AnnotationBody.js
+++
@@ -1,7 +0,0 @@
-goog.provide('atb.AnnotationBody');
-/** @deprecated */
-atb.AnnotationBody = function (resourceId, type, user) {
-	this.resourceId = resourceId;
-	this.user = user;
-	this.type = type;
-};
