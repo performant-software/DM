f9835c5aa1c11ff4224d646622cc08bd65016b03
Tim Andres <tandres@drew.edu>
2013-02-10 18:23:18 -0500

    Changed RepoBrowser options format
    There was some confusion as to what was actually to be specified to the
    RepoBrowser as the information for the original repositories. The
    format of the RepoBrowser constructor options was changed so that it
    takes an array of objects with information about each repository,
    including a title, url of the file where the repo's manifest can be
    found, and the uri of the repo.

--- workspace/static/js/atb/Util.js
+++ workspace/static/js/atb/Util.js
@@ -23,6 +23,37 @@ atb.Util.scopeAsyncHandler = function(handler, opt_scope) {
 }
 
 /**
+ * Implementation of Function#bind from 
+ * https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/bind
+ * for pre-ECMA Script 5 browsers.
+ *
+ * For code readability, the .bind(this) syntax should be used rather than {atb.Util.scopeAsyncHandler}
+ */
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+ 
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+ 
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+ 
+    return fBound;
+  };
+}
+
+/**
  * Executes functions in a sequence of timeouts (useful for animation queues)
  * Delay times are specified in milliseconds, and can be either one time, or an array of times
  * (but the array size must be the same as the array of functions to execute).
