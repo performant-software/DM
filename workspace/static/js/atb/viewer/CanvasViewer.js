69966c9696dc3d88e57b35e7190a4514149e6826
Shannon Bradshaw <shannon.bradshaw@gmail.com>
2013-02-17 23:38:14 -0500

    Needed to resolve conflicts.

--- workspace/static/js/atb/viewer/CanvasViewer.js
+++ workspace/static/js/atb/viewer/CanvasViewer.js
@@ -329,12 +329,6 @@ atb.viewer.CanvasViewer.prototype.showAnnos = function (opt_uri) {
 };
 
 atb.viewer.CanvasViewer.prototype.createTextAnno = function(uri) {
-<<<<<<< HEAD
-    var canvasUri = this.getUri();
-    var canvasResource = this.databroker.getResource(this.getUri());
-    
-    var canvasTitle = canvasResource.getOneProperty('dc:title') || 'Untitled canvas';
-=======
     console.log("createTextAnno uri:", uri);
     var id = this.webService.resourceUriToId(uri);
     var svgUri = sc.util.Namespaces.wrapWithAngleBrackets(uri);
@@ -344,7 +338,6 @@ atb.viewer.CanvasViewer.prototype.createTextAnno = function(uri) {
     
     var canvasTitle = canvasResource.getOneProperty('dc:title') || 'Untitled canvas';
     
->>>>>>> creating a text annotation from hover menu now creates the correct annotation targeting the specific resource that identifies the canvas as source and svg as selector (body is the new text created).
     var textTitle = 'New annotation on ' + canvasTitle;
     
     var textEditor = new atb.viewer.Editor(this.clientApp);
@@ -353,9 +346,6 @@ atb.viewer.CanvasViewer.prototype.createTextAnno = function(uri) {
     var newTextResource = this.databroker.createText(textTitle, "");
     var newTextId = newTextResource.uri;
 
-<<<<<<< HEAD
-    var newAnno = this.databroker.createAnno(newTextId, uri, null);
-=======
     // This should be a convenience method in the data broker
     var specificTargets = [];
     this.databroker.quadStore.forEachQuadMatchingQuery(
@@ -367,7 +357,6 @@ atb.viewer.CanvasViewer.prototype.createTextAnno = function(uri) {
     );
        
     var newAnno = this.databroker.createAnno(newTextId, specificTargets[0]);
->>>>>>> creating a text annotation from hover menu now creates the correct annotation targeting the specific resource that identifies the canvas as source and svg as selector (body is the new text created).
     var annoId = newAnno.uri;
 
     textEditor.resourceId = newTextId;
