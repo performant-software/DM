a8fec42c5a778a003fdbaee043bf066f02d7f08a
Tim Andres <tandres@drew.edu>
2013-03-19 23:12:10 -0400

    Added viewer titles and close button

--- workspace/static/js/atb/viewer/ViewerContainer.js
+++ workspace/static/js/atb/viewer/ViewerContainer.js
@@ -1,7 +1,7 @@
 goog.provide('atb.viewer.ViewerContainer');
 
 goog.require('goog.dom.DomHelper');
-goog.require('goog.events.EventTarget')
+goog.require('goog.events.EventTarget');
 goog.require('jquery.jQuery');
 
 atb.viewer.ViewerContainer = function(opt_domHelper) {
@@ -11,13 +11,27 @@ atb.viewer.ViewerContainer = function(opt_domHelper) {
     this.grid = null;
     this.domHelper = opt_domHelper || new goog.dom.DomHelper();
 
-    this.element = this.domHelper.createDom('div', {'class': 'atb-ViewerContainer'});
-    this.titleEl = this.domHelper.createDom('h3', {'class': 'atb-ViewerContainer-title'});
-    this.viewerEl = this.domHelper.createDom('div', {'class': 'atb-ViewerContainer-viewer'});
-    this.element.appendChild(this.titleEl);
-    this.element.appendChild(this.viewerEl);
+    this._setupDom();
 };
 goog.inherits(atb.viewer.ViewerContainer, goog.events.EventTarget);
+
+atb.viewer.ViewerContainer.prototype._setupDom = function() {
+    this.element = this.domHelper.createDom('div', {'class': 'atb-ViewerContainer'});
+
+    this.titleWrapper = this.domHelper.createDom('div', {'class': 'atb-ViewerContainer-titleWrapper'});
+    this.titleEl = this.domHelper.createDom('h3', {'class': 'atb-ViewerContainer-title'});
+    this.closeButton = this.domHelper.createDom('div', {'class': 'icon-remove-sign atb-ViewerContainer-close'});
+
+    goog.events.listen(this.closeButton, 'click', this.close, false, this);
+
+    this.viewerEl = this.domHelper.createDom('div', {'class': 'atb-ViewerContainer-viewer'});
+
+    this.titleWrapper.appendChild(this.closeButton);
+    this.titleWrapper.appendChild(this.titleEl);
+    
+    this.element.appendChild(this.titleWrapper);
+    this.element.appendChild(this.viewerEl);
+};
 
 atb.viewer.ViewerContainer.prototype.setViewer = function(viewer) {
     if (this.viewer) {
@@ -44,6 +58,8 @@ atb.viewer.ViewerContainer.prototype.getDomHelper = function() {
 
 atb.viewer.ViewerContainer.prototype.setTitle = function(title) {
     this.title = title;
+
+    jQuery(this.titleEl).text(title);
 };
 
 atb.viewer.ViewerContainer.prototype.getTitle = function() {
@@ -63,7 +79,7 @@ atb.viewer.ViewerContainer.prototype.autoResize = function() {
         var $el = jQuery(this.element);
 
         var width = $el.innerWidth();
-        var height = $el.innerHeight() - jQuery(this.titleEl).outerHeight();
+        var height = $el.innerHeight() - jQuery(this.titleWrapper).height();
 
         this.viewer.resize(width, height);
     }
