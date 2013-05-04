55580ef3d80d6580af1b89eab746cac652c39a6d
Shannon Bradshaw <shannon.bradshaw@gmail.com>
2013-05-04 14:24:41 -0400

    new dummy data

--- manage_command_examples.sh
+++ manage_command_examples.sh
@@ -8,6 +8,8 @@ django-admin.py harvest_collection_manifests --uri=http://manifests.ydc2.yale.ed
 
 django-admin.py harvest_collection_manifests --uri=http://ada.drew.edu/tandres/repos/SharedCanvas/Main/Collection --url http://ada.drew.edu/tandres/repos/SharedCanvas/Main/Collection.xml --manifest_file /Users/shannon/python_lib/dm/semantic_store/dummy_data/SharedCanvasCollection.xml --store_host "localhost:8081" --rep_uri http://shared-canvas.org/Repository --rep_title "Shared-canvas.org Demos" --settings=dm.settings
 
+django-admin.py harvest_collection_manifests --uri=http://dms-data.stanford.edu/Parker/Collection --url http://dms-data.stanford.edu/Parker/Collection.xml --manifest_file /Users/shannon/python_lib/dm/semantic_store/dummy_data/ParkerMini.xml --store_host "localhost:8081" --rep_uri http://dms-data.stanford.edu/Repository --rep_title "Manuscripts Hosted by the Stanford University Libraries" --settings=settings
+
 
 django-admin.py create_user --username testuser --email testuser@gmail.com --password "m@ggie" --store_host localhost:8081 --settings=dm.settings
 
