72c229f88d9b8e938d6684663908ca8b6c8b7517
Lucy <thecoloryes@gmail.com>
2013-06-06 09:40:54 -0400

    Create Project full functionality, some display project titles functionality, additional cleanup
    Cleanups: correctly rendering user urls as rdf; reduced load on
    rendering project urls

--- semantic_store/projects.py
+++ semantic_store/projects.py
@@ -5,23 +5,31 @@ from django.conf import settings
 from django.db import transaction
 
 from rdflib import Graph, URIRef, Literal
-from .namespaces import NS
+from semantic_store.namespaces import NS, bind_namespaces
 from semantic_store import uris
 
 from semantic_store.rdfstore import rdfstore
 
-def create_project_graph(g, node, identifier, host, user_email=None):
+def create_project_graph(g, node, identifier, host, title, user_email=None):
     with transaction.commit_on_success():
         allprojects_uri = uris.uri('semantic_store_projects')
         allprojects_g = Graph(store=rdfstore(), identifier=allprojects_uri)
+        bind_namespaces(allprojects_g)
+        
+        #print "all projects", allprojects_g.serialize()
 
-        uri = uris.uri('semantic_store_projects', identifier=identifier)
+        uri = uris.uri('semantic_store_projects', uri=identifier)
         project_g = Graph(store=rdfstore(), identifier=uri)
+        bind_namespaces(project_g)
+        #print "new project", project_g.serialize()
 
         for s, p, o in g.triples((node, NS.rdf['type'], None)):
             allprojects_g.add((identifier, p, o))
-        url = uris.url(host, 'semantic_store_projects', identifier=identifier)
+        url = uris.url(host, 'semantic_store_projects', uri=identifier)
         allprojects_g.add((identifier, NS.ore['isDescribedBy'], url))
+        #print "all projects", allprojects_g.serialize()
+
+        allprojects_g.add((identifier, NS.dc['title'], Literal(title)))
 
         project_g.add((identifier, NS.ore['isDescribedBy'], url))
         project_g.add((identifier, NS.dcterms['created'], Literal(datetime.utcnow())))
@@ -29,10 +37,28 @@ def create_project_graph(g, node, identifier, host, user_email=None):
         if user_email:
             project_g.add((identifier, NS.dcterms['creator'], URIRef(user_email)))
 
+        #print "new project", project_g.serialize()
+
         for s, p, o in g.triples((node, None, None)):
             project_g.add((identifier, p, o))
         for agg in g.objects(node, NS.ore['aggregates']):
             for t in g.triples((agg, None, None)):
                 project_g.add(t)
+
+        #print "all projects", allprojects_g.serialize()
+        #print "new project", project_g.serialize()
         return project_g
-    
+
+
+# Creates a graph identified by user of the projects belonging to the user, which
+# # can be found at the descriptive url of the user (/store/user/<username>)
+# The graph houses the uri of all of the user's projects and the url where more info
+# # can be found about each project
+def create_project_user_graph(host, user, project):
+    with transaction.commit_on_success():
+        identifier = uris.uri('semantic_store_users', username=user)
+        g = Graph(store=rdfstore(), identifier = identifier)
+        bind_namespaces(g)
+        g.add((identifier, NS.ore['aggregates'], project))
+        g.add((project, NS.ore['isDescribedBy'], uris.url(host, "semantic_store_projects", uri=project)))
+        return g.serialize()
--- semantic_store/urls.py
+++ semantic_store/urls.py
@@ -6,49 +6,76 @@ urlpatterns = patterns('',
     url(r'^resources(?:/(?P<uri>.+))?/?$', 
         semantic_store.views.resources, 
         name="semantic_store_resources"),
+
+
     url(r'^repositories(?:/(?P<uri>.+))?/?$', 
         semantic_store.views.repositories, 
         name="semantic_store_repositories"),
+
     url(r'^repositories/(?P<uri>.+)/annotations/?$', 
         semantic_store.views.repositories, 
         name="semantic_store_repositories"),
+
+
     url(r'^projects/(?P<project_uri>.+)/annotations(?:/(?P<anno_uri>.+))?/?$', 
         semantic_store.views.project_annotations, 
         name="semantic_store_project_annotations"),
+
     url(r'^projects/(?P<identifier>.+)/texts(?:/(?P<text_identifier>.+))?/?$', 
         ProjectTextView.as_view(), 
         name="semantic_store_project_texts"),
+
     url(r'^projects/(?P<identifier>.+)/resources(?:/(?P<resource_identifier>.+))?/?$', 
         semantic_store.views.projects, 
+        name="semantic_store_project_resources"),
+
+    url(r'^projects(?:/(?P<uri>.+)/)?/?$', 
+        semantic_store.views.projects,
         name="semantic_store_projects"),
-    url(r'^projects(?:/(?P<identifier>.+))?/?$', 
-        ProjectView.as_view(), 
+
+    url(r'^projects(?:/(?P<uri>.+))?/?$', 
+        semantic_store.views.projects,
         name="semantic_store_projects"),
+
+
     url(r'^manuscripts(?:/(?P<uri>.+))?/?$', 
         semantic_store.views.manuscripts, 
         name="semantic_store_manuscripts"),
+
     url(r'^manuscripts/(?P<uri>.+)/annotations/?$', 
         semantic_store.views.manuscript_annotations, 
         name="semantic_store_manuscripts"),
+
+
     url(r'^manuscript_collections(?:/(?P<uri>.+))/?$', 
         semantic_store.views.manuscript_collections, 
         name="semantic_store_manuscript_collections"),
+
     url(r'^manuscript_collections/(?P<uri>.+)/annotations/?$', 
         semantic_store.views.manuscript_collection_annotations, 
-        name="semantic_store_project"),
+        name="semantic_store_manuscript_collection_annotations"),
+
+
     url(r'^collections(?:/(?P<uri>.+))/?$', 
         semantic_store.views.collections, 
         name="semantic_store_collections"),
+
     url(r'^collections/(?P<uri>.+)/annotations/?$', 
         semantic_store.views.collection_annotations, 
-        name="semantic_store_project"),
+        name="semantic_store_collection_annotations"),
+
+
     url(r'^users(?:/(?P<username>.+))?/?$', 
         UserView.as_view(), 
         name="semantic_store_users"),
+
     url(r'^users/(?P<username>.+)/annotations/?$', 
         UserView.as_view(), 
         name="semantic_store_user_annotations"),
+
+
     url(r'^annotations(?:/(?P<anno_uri>.+))?/?$', 
         semantic_store.views.annotations, 
         name="semantic_store_annotations"),
+
 )
