9deba232475046b7d66b90e4e2dbb1ba7553fb5f
Tim Andres <tandres@drew.edu>
2013-06-25 16:19:47 -0400

    Fixed back end text bugs

--- semantic_store/views.py
+++ semantic_store/views.py
@@ -22,6 +22,14 @@ from .annotation_views import create_or_update_annotations, get_annotations, \
 from .projects import create_project_from_request, create_project, read_project, update_project, delete_triples_from_project
 from semantic_store import uris
 
+from project_texts import (
+    create_project_text_from_request,
+    read_project_text,
+    update_project_text_from_request,
+    remove_project_text
+)
+
+
 from django.contrib.auth.models import User
 from django.db import IntegrityError, transaction
 
@@ -262,7 +270,7 @@ def project_texts(request, project_uri, text_uri):
         return negotiated_graph_response(request, g)
 
     elif request.method == 'DELETE':
-        g = delete_project_text(project_uri, text_uri)
+        g = remove_project_text(project_uri, text_uri)
         return negotiated_graph_response(request, g)
 
 
