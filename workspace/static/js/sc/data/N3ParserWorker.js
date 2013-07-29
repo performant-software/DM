8ef2ea0e13bc827b531ca6278fdb54ce17701791
Tim Andres <tandres@drew.edu>
2013-07-29 15:25:43 -0400

    Offloaded uri wrapping in N3 Parser to worker process, and corrected its error handling when web workers are not available

--- workspace/static/js/sc/data/N3ParserWorker.js
+++ workspace/static/js/sc/data/N3ParserWorker.js
@@ -7,13 +7,38 @@ importScripts(STATIC_URL + 'js/n3/n3lexer.js');
 importScripts(STATIC_URL + 'js/n3/n3store.js');
 importScripts(STATIC_URL + 'js/n3/n3parser.js');
 
+var isBNode = function(str) {
+    return str.substring(0, 2) == '_:';
+};
+
+var wrap = function(str) {
+    if (str[0] != '"' && !isBNode(str)) {
+        return ['<', str.replace(/>/g, '\\>'), '>'].join('');
+    }
+    else {
+        return str;
+    }
+}
+
 var parser = new N3Parser();
 
 addEventListener('message', function(e) {
     parser.parse(e.data, function(error, triple) {
-        postMessage({
-            'error': error,
-            'triple': triple
-        });
+        if (triple) {
+            postMessage({
+                'error': error,
+                'triple': {
+                    'subject': wrap(triple.subject),
+                    'predicate': wrap(triple.predicate),
+                    'object': wrap(triple.object)
+                }
+            });
+        }
+        else {
+            postMessage({
+                'error': error,
+                'triple': null
+            })
+        }
     });
 });
