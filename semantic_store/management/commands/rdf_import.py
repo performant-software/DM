8599edb725d7ffc946e72f27753b86ea168327de
Tim Andres <tandres@drew.edu>
2013-10-07 23:42:55 -0400

    Introduced a hack to compensate for django's url reverse function returning different values between the management commands and the production server when running with a script name other than '/'

--- semantic_store/management/commands/rdf_import.py
+++ semantic_store/management/commands/rdf_import.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.db import transaction, IntegrityError
+from django.conf import settings
 from optparse import make_option
 import os
 
@@ -55,6 +56,11 @@ class Command(BaseCommand):
                 print "User '%s' was created successfully." % username
 
     def handle(self, filename, directory, *args, **options):
+        # The following is a regrettable hack to fix the fact that the uris we use as graph identifiers change based on whether
+        # they are being generated from the shell or from a live production server like gunicorn at a non-root url
+        if settings.FORCE_SCRIPT_NAME:
+            settings.STORE_HOST += settings.FORCE_SCRIPT_NAME
+
         if filename:
             self.parse_file(os.path.join(os.getcwd(), filename))
 
