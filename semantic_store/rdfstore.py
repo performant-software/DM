d7ea708eff03613c74fd5e6ca7e03a016158df9a
Tim Andres <tandres@drew.edu>
2013-11-11 20:55:00 -0500

    rdfstore() now just maintains one open connection

--- semantic_store/rdfstore.py
+++ semantic_store/rdfstore.py
@@ -2,9 +2,12 @@ from rdflib import plugin, URIRef, Literal
 from rdflib.store import Store
 from django.conf import settings
 
-default_identifier = URIRef(settings.RDFLIB_STORE_GRAPH_URI)        
+plugin.register('SQLAlchemy', Store, 'rdflib_sqlalchemy.SQLAlchemy', 'SQLAlchemy')
+
+default_identifier = URIRef(settings.RDFLIB_STORE_GRAPH_URI)   
+
+store = plugin.get('SQLAlchemy', Store)(identifier=default_identifier)
+store.open(Literal(settings.RDFLIB_DB_URI))     
 
 def rdfstore():
-    store = plugin.get('SQLAlchemy', Store)(identifier=default_identifier)
-    store.open(Literal(settings.RDFLIB_DB_URI))
     return store
