ee7c7138f7eedc183368fc2bce90ef4eda4c3e7a
Tim Andres <tandres@drew.edu>
2014-01-12 20:24:38 -0500

    Fixed a significant typo in uris.add_is_described_bys

--- semantic_store/uris.py
+++ semantic_store/uris.py
@@ -25,20 +25,20 @@ def project_metadata_graph_identifier(project_uri):
 
 def add_is_described_bys(graph, project_uri):
     for uri in graph.subjects(NS.rdf.type, NS.sc.Canvas):
-        canvas_url = URIRef(url('semantic_store_project_canvases', project_uri=project_uri, canvas_uri=uri))
+        canvas_url = url('semantic_store_project_canvases', project_uri=project_uri, canvas_uri=uri)
         graph.add((uri, NS.ore.isDescribedBy, canvas_url))
 
-    for uri in graph.subjects(NS.rdf.type, NS.dc.Text):
-        text_url = URIRef(url('semantic_store_project_texts', project_uri=project_uri, text_uri=uri))
+    for uri in graph.subjects(NS.rdf.type, NS.dcmitype.Text):
+        text_url = url('semantic_store_project_texts', project_uri=project_uri, text_uri=uri)
         graph.add((uri, NS.ore.isDescribedBy, text_url))
 
     for uri in graph.subjects(NS.rdf.type, NS.oa.SpecificResource):
         source = graph.value(uri, NS.oa.hasSource, None)
         if (source, NS.rdf.type, NS.sc.Canvas) in graph:
-            canvas_url = URIRef(url("semantic_store_canvas_specific_resource", project_uri=project_uri, canvas_uri=source, specific_resource=uri))
+            canvas_url = url("semantic_store_canvas_specific_resource", project_uri=project_uri, canvas_uri=source, specific_resource=uri)
             graph.add((uri, NS.ore.isDescribedBy, canvas_url))
         elif (source, NS.rdf.type, NS.dcmitype.Text) in graph:
-            text_url = URIRef(url("semantic_store_text_specific_resource", project_uri=project_uri, text_uri=source, specific_resource=uri))
+            text_url = url("semantic_store_text_specific_resource", project_uri=project_uri, text_uri=source, specific_resource=uri)
             graph.add((uri, NS.ore.isDescribedBy, text_url))
 
     return graph
