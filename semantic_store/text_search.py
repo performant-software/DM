977757aa6f0465291d230701ae081fe85e7d4f29
Tim Andres <tandres@drew.edu>
2014-01-21 20:41:25 -0500

    Modified the search web service API to allow the delivery of n3 to be turned off (for performance improvements)

--- semantic_store/text_search.py
+++ semantic_store/text_search.py
@@ -5,6 +5,7 @@ from rdflib import Graph, URIRef, Literal
 
 from semantic_store import uris, project_texts, utils, projects, annotations
 from semantic_store.models import Text
+from semantic_store.namespaces import NS
 
 def search_result_to_dict(result, project_uri):
     stored_fields = result.get_stored_fields()
@@ -12,6 +13,7 @@ def search_result_to_dict(result, project_uri):
     d = {
         'uri': stored_fields['identifier'],
         'url': uris.url('semantic_store_project_texts', project_uri=project_uri, text_uri=stored_fields['identifier']),
+        'type': NS.dctypes.Text,
         'title': stored_fields['title'],
         'score': result.score,
         'highlighted': result.highlighted,
@@ -19,7 +21,7 @@ def search_result_to_dict(result, project_uri):
 
     return d
 
-def get_response(project_uri, query_string):
+def get_response(project_uri, query_string, include_n3=True):
     d = {
         'results': list(),
     }
@@ -38,8 +40,11 @@ def get_response(project_uri, query_string):
 
         if annotations.has_annotation_link(project_graph, text_uri) or projects.is_top_level_project_resource(project_uri, text_uri):
             d['results'].append(search_result_to_dict(result, project_uri))
-            graph += utils.metadata_triples(project_graph, text_uri)
 
-    d['n3'] = graph.serialize(format='n3')
+            if include_n3:
+                graph += utils.metadata_triples(project_graph, text_uri)
+
+    if include_n3:
+        d['n3'] = graph.serialize(format='n3')
 
     return d
