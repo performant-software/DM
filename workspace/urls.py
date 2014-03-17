26141f1dcd7cb6a0ebfd6203ba972febef156a5d
Lucy <thecoloryes@gmail.com>
2014-03-17 11:37:23 -0400

    Added base page for linking images to projects

--- workspace/urls.py
+++ workspace/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
     url(r"^project_forward/$", "semantic_store.views.projects"),
     url(r"^$", 'workspace.views.workspace', name='workspace'),
 
-    url(r'^upload_image/$', "workspace.views.upload_image_view", name="upload_image")
+    url(r'^add_image/$', "workspace.views.add_image", name="add_image"),
+    url(r'^upload_image/$', "workspace.views.upload_image_view", name="upload_image"),
 
 )
