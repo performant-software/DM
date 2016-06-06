14c002c41f0784384baef317975258cebc5ead59
Gregor Middell <gregor@middell.net>
2016-06-06 13:54:28 +0200

    Interactve Scroll Sample: import

---
+++ src/main/java/edu/drew/dm/InteractiveScrollImport.java
@@ -0,0 +1,66 @@
+package edu.drew.dm;
+
+import edu.drew.dm.vocabulary.OpenArchivesTerms;
+import edu.drew.dm.vocabulary.Perm;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.vocabulary.RDF;
+
+import java.io.File;
+import java.io.FilterWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * @author <a href="http://gregor.middell.net/">Gregor Middell</a>
+ */
+public class InteractiveScrollImport {
+
+    public static void main(String[] args) {
+        final File dataDir = Stream.of(args).map(File::new).filter(File::isDirectory).findFirst().orElseThrow(IllegalArgumentException::new);
+
+        final Model model = Models.create();
+
+        final File[] tripleFiles = dataDir.listFiles(f -> {
+            return f.getName().endsWith(".ttl");
+        });
+
+        for (File tripleFile : tripleFiles) {
+            model.read(tripleFile.toURI().toString());
+        }
+
+        model.removeAll(null, OpenArchivesTerms.isDescribedBy, null);
+
+        Models.renameResources(model, (Function<Resource, String>) resource -> {
+            try {
+                final String uri = resource.getURI();
+                return uri.startsWith("file:") ? new URI("image", uri.substring(uri.lastIndexOf("/") + 1), null).toString() : uri;
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        model.listSubjectsWithProperty(RDF.type, FOAF.Project)
+                .forEachRemaining(project -> model.add(
+                        model.createResource("user:lou"),
+                        Perm.hasPermissionOver,
+                        project
+                ));
+
+        try (final FilterWriter writer = new FilterWriter(new PrintWriter(System.out)) {
+            @Override
+            public void close() throws IOException {
+                this.out.flush();
+            }
+        }) {
+            model.write(writer, "N3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
