f29e9709add053427d6dc9233ac74b78fb435066
Gregor Middell <gregor@middell.net>
2017-05-16 11:32:40 +0200

    Refactor build

--- README.md
+++ README.md
@@ -2,32 +2,30 @@ DM: Tools For Digital Annotation and Linking
 ============================================
 (Formerly Digital Mappaemundi)
 
+Prerequisites
+-------------
+
+* Python v2
+* Java Development Kit v8
+* [Apache Maven](http://maven.apache.org/)
+* [NodeJS](https://nodejs.org/)
+* [Yarn](https://yarnpkg.com/)
 
 Build instructions
 ------------------
 
-In order to build client-side assets via [NodeJS](https://nodejs.org/):
-
-    $ npm install
-    $ npm run build
-
-In order to build the Java-based server component (including client-side assets):
-
-    $ mvn clean package
-
-(Java Development Kit v8 and [Apache Maven](http://maven.apache.org/) needed.)
+    $ scripts/build
 
 Development instructions
 ------------------------
 
 In order to develop the client-side code, build and start the backend:
 
-    $ mvn clean package
-    $ java -jar target/dm-<version>.jar
+    $ scripts/build
+    $ scripts/run
 
 In parallel, initialize and start [Browsersync](https://www.browsersync.io/):
 
-    $ npm install
     $ npm run dev
 
 Browsersync serves client-side assets from `src/main/resources/static` and
---
+++ scripts/frontend-dev.js
@@ -0,0 +1,36 @@
+/* global require */
+const browserSync = require("browser-sync").create();
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const stripAnsi = require("strip-ansi");
+const proxy = require("http-proxy-middleware");
+
+const webpackConfig = require("../webpack.config");
+const bundler = webpack(webpackConfig);
+
+bundler.plugin("done", function(stats) {
+    if (stats.hasErrors() || stats.hasWarnings()) {
+        browserSync.sockets.emit("fullscreen:message", {
+            title: "Webpack Error:",
+            body: stripAnsi(stats.toString()),
+            timeout: 100000
+        });
+    }
+    browserSync.reload();
+});
+
+browserSync.init({
+    server: {
+        baseDir: "src/main/resources",
+        directory: true,
+        middleware: [
+            webpackDevMiddleware(bundler, {
+                publicPath: webpackConfig.output.publicPath,
+                stats: { colors: true }
+            }),
+            proxy(["**", '!/static/**'], {target: "http://localhost:8080"})
+        ]
+    },
+    files: ["src/main/resources/static"],
+    plugins: ["bs-fullscreen-message"]
+});
